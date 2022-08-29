6d18be5539fe89989f3969300b886b0a0dd72a87
Nebja <benjaminkotsaridis@nebja.eu>
2022-08-29 17:01:44 +0300

    ttest finish

--- deploy.php
+++ deploy.php
@@ -2,6 +2,7 @@
 namespace Deployer;
 
 require 'recipe/symfony.php';
+require 'contrib/yarn.php';
 
 // Config
 
@@ -9,6 +10,7 @@ set('repository', 'https://github.com/Nebja/testing.git');
 
 add('shared_files', []);
 add('shared_dirs', []);
+set('keep_releases', 3);
 add('writable_dirs', []);
 
 // Hosts
@@ -28,8 +30,29 @@ host('test-prod')
     ->setHostname('192.168.226.3')
     ->setRemoteUser('nebja')
     ->setPort(22)
-    ->set('deploy_path', '/var/www/test.localnet/prod/webroot');
+    ->setDeployPath('/var/www/test.localnet/prod/webroot');
 
 // Hooks
-
 after('deploy:failed', 'deploy:unlock');
+
+//tasks
+task( 'deploy', [
+    'deploy:setup',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'yarn:install',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:vendors',
+    'deploy:cache:clear',
+    'deploy:symlink',
+    'deploy:unlock',
+    'deploy:cleanup',
+    'deploy:success',
+    'build'
+]);
+task('build', function (){
+    cd('/var/www/test.localnet/dev/webroot/current');
+    run('yarn run build');
+});
