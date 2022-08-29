d4bad13ac5ba06de307fb9b7d2a642b4b8e70b8b
Nebja <benjaminkotsaridis@nebja.eu>
2022-08-29 15:57:37 +0300

    test

---
+++ assets/vue-templates/home.vue
@@ -0,0 +1,25 @@
+<template>
+ <p>  {{ trans.Hello }}</p>
+ <p>{{ trans.newmsg}}</p>
+  <p>{{ trans.frmsg}}</p>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return{
+      trans: ''
+    }
+  },
+  mounted() {
+    this.trans = JSON.parse(document.getElementById("app").getAttribute('trans'))
+    console.log(this.trans)
+    console.log('hello')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
